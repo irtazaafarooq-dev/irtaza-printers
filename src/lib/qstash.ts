408292f239b38ebe57411cdc883b0b2d6f39d1d5
Muhammad Irtaza Farooq <2023CS623@student.uet.edu.pk>
2026-08-07 02:40:24 +0500

    qstash file update

--- src/lib/qstash.ts
+++ src/lib/qstash.ts
@@ -1,5 +1,10 @@
 import { Client } from "@upstash/qstash";
 
 export const qstashClient = new Client({
+  baseUrl: process.env.QSTASH_BASE_URL!,
   token: process.env.QSTASH_TOKEN!,
-});
+})
+
+await qstashClient.publish({
+  url: "https://irtazaprinters.com",
+})
